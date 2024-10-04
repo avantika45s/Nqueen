2715a68c959e47b1d0dc477ba0c24cd058df522e
Avantika <141184739+avantika45s@users.noreply.github.com>
2024-10-04 23:20:44 +0530

    Create Nqueen.cpp

---
+++ Nqueen.cpp
@@ -0,0 +1,67 @@
+//N queen probelm in c++ using recursion
+
+
+
+
+
+
+
+class Solution {
+public:
+
+bool isSafe(int row, int col, vector<string>& board, int n) {
+        int duprow = row;
+        int dupcol = col;
+
+        // Check upper left diagonal
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        // Check left side
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        // Check lower left diagonal
+        while (row < n && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+
+    void solve(int col, vector<string>& board, int n, vector<vector<string>>& ans) {
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solve(col + 1, board, n, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        solve(0, board, n, ans);
+        return ans;
+    }
+};
